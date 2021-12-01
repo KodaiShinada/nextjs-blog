fed30cba30eb26a243ae02f5ccd95fcb0b4454f5
kodais <82872034+KodaiShinada@users.noreply.github.com>
2021-12-02 04:00:02 +0900

    Update file structure

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Kodai Shinada's Blog</title>
+        <link
+          rel="icon"
+          href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/310/tangerine_1f34a.png"
+          />
+      </Head>
+      <main>Hello! My name is Kodai.</main>
+    </div>
+  );
+}
