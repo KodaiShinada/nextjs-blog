a1923ba607709de3889dccfb7f9c5e91f20f7756
kodais <82872034+KodaiShinada@users.noreply.github.com>
2021-12-04 23:50:45 +0900

    upload some test files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import { GetStaticProps } from 'next'
 
 export default function Home() {
   return (
@@ -8,9 +9,15 @@ export default function Home() {
         <link
           rel="icon"
           href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/310/tangerine_1f34a.png"
-          />
+        />
       </Head>
       <main>Hello! My name is Kodai.</main>
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {},
+  };
+};
